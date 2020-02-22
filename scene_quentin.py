26381048c6b4aee63294c7dab2d1eb357e6ca8ec
Nolwenn Lévêque <nol.leveuqe@outlook.com>
2020-02-22 23:01:24 +0100

    animation du corps en fonction de la puissance

--- scene_quentin.py
+++ scene_quentin.py
@@ -19,6 +19,7 @@ class PlayerAnimation(Enum):
     NONE = 0
     RUN = 1
     JUMP = 2
+    REST = 3
 
 
 
@@ -26,7 +27,7 @@ class Player(CompositeItem):
     def __init__(self, camera: Camera, pos: Vector2):
         super().__init__(camera, pos, Vector2(0.1, 0.1))
 
-        self._power = 0.0
+        self._power = 4
         self._T = None
         self._t = 0
         self._BASE_BODY_SIZE = Vector2(0.06, 0.08)
@@ -36,38 +37,46 @@ class Player(CompositeItem):
         self._max_leg_rotation = 40
         self._max_arm_rotation = 40
 
-        #jump
+        # ----------- initial body shape -------------
+        self._muscle_level = 0
+        self._image_body = 'body_m.png'
+        self._image_right_arm = 'right_arm_w.png'
+        self._image_left_arm = 'left_arm_w.png'
+        self._image_left_leg = 'left_leg.png'
+        self._image_right_leg = 'right_leg.png'
+
+        # ----------- jump management ---------------
         self._z = 0
         self._ground = self.pos.y
         self._v_speed = 0.0
 
         color = (100, 100, 100)
 
-        self._body = ImageItem(camera, Vector2(0, 0), self._BASE_BODY_SIZE, image='body_m.png')
+        self._body = ImageItem(camera, Vector2(0, 0), self._BASE_BODY_SIZE, image=self._image_body)
         self._add_item(self._body)
 
         self._left_leg = ImageItem(camera,
                                    self._compute_leg_position(False),
                                    self._BASE_LEG_SIZE,
-                                   image='left_leg.png')
+                                   image=self._image_left_leg)
         self._add_item(self._left_leg)
 
         self._right_leg = ImageItem(camera,
                                     self._compute_leg_position(True),
                                     self._BASE_LEG_SIZE,
-                                    image='right_leg.png')
+                                    image=self._image_right_leg)
         self._add_item(self._right_leg)
 
         self._right_arm = ImageItem(camera,
                                     self._compute_arm_position(True),
                                     self._BASE_ARM_SIZE,
-                                    image='right_arm_m.png')
+                                    image=self._image_right_arm)
         self._add_item(self._right_arm)
 
         self._left_arm = ImageItem(camera,
                                    self._compute_arm_position(False),
                                    self._BASE_ARM_SIZE,
-                                   image='left_arm_m.png')
+                                   image=self._image_left_arm)
         self._add_item(self._left_arm)
 
         self._set_animation(PlayerAnimation.RUN)
@@ -99,7 +108,9 @@ class Player(CompositeItem):
             self._T = 25
         elif self._animation == PlayerAnimation.JUMP:
             self._max_leg_rotation = 60
-            self._t = 0
+            self._t = 10
+        elif self._animation == PlayerAnimation.REST:
+            self._t = 5
 
 
     def draw(self):
@@ -111,14 +122,17 @@ class Player(CompositeItem):
         if self._t == self._T:
             self._t = 0
 
+        # ----------- move ---------------
+        factor_speed = 7-self._power
         if self._right and not self._left:
-            self._speed = Vector2(1, 0)
+            self._speed = Vector2(1, 0)*factor_speed
         elif self._left and not self._right:
-            self._speed = Vector2(-1, 0)
+            self._speed = Vector2(-1, 0)*factor_speed
         else:
             self._speed *= 0.9
             if self._speed.length() < 0.001:
                 self._speed = Vector2(0, 0)
+            self._set_animation(PlayerAnimation.REST)
 
         print(self._z)
         if self._z > 0:
@@ -127,11 +141,13 @@ class Player(CompositeItem):
 
         # ----------- jump ---------------
         if self._up and self._z <= 0:
-            self._v_speed = 1.0
+            factor_jump_height = 5-self._power
+            self._v_speed = 1.0 * factor_jump_height
             self._set_animation(PlayerAnimation.JUMP)
 
-        # ---------- lay down ------------
         self._z += self._v_speed * 0.005
+
+        # ---------- lay down ------------
         if self._z < 0:
             self._z = 0
             self._v_speed = 0
@@ -147,10 +163,8 @@ class Player(CompositeItem):
         self._left_leg.set_rotation(-self._max_leg_rotation * math.cos(lambda_ * 2 * math.pi))
         self._right_leg.set_rotation(self._max_leg_rotation * math.cos(lambda_ * 2 * math.pi))
 
-        factor = self._power + 1
-        arm_size = self._BASE_ARM_SIZE * factor
-        leg_size = self._BASE_LEG_SIZE
-        body_size = self._BASE_BODY_SIZE * factor
+        self.update_body_size()
+
         super().update()
 
     def set_right(self, right):
@@ -161,6 +175,43 @@ class Player(CompositeItem):
 
     def set_up(self, up):
         self._up = up
+
+    def update_body_size(self):
+        factor = self._power
+
+        # thresholds
+        muscle_level_m = 5
+        muscle_level_s = 8
+
+        # muscle level - update images
+        if self._power > muscle_level_s:
+            new_muscle_level = 2
+        elif self._power > muscle_level_m:
+            new_muscle_level = 1
+        else:
+            new_muscle_level = 0
+
+        if new_muscle_level != self._muscle_level:
+            # right_arm
+            right_arms = ['right_arm_w.png', 'right_arm_m.png', 'right_arm_s.png']
+            self._right_arm.load_image(right_arms[new_muscle_level])
+            # left_arm
+            left_arms = ['left_arm_w.png', 'left_arm_m.png', 'left_arm_s.png']
+            self._left_arm.load_image(left_arms[new_muscle_level])
+            # body
+            bodies = ['body_w.png', 'body_m.png', 'body_s.png']
+            self._body.load_image(bodies[new_muscle_level])
+
+            self._muscle_level = new_muscle_level
+
+
+        # body size
+        body_size = self._BASE_BODY_SIZE * factor
+        arm_size = self._BASE_ARM_SIZE * factor
+        leg_size = self._BASE_LEG_SIZE
+
+
+
 
 
 class SceneQuentin(Scene):
